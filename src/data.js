89c42838108d61877c5d129ad1ecd106bb5fee29
Jeff.vieira <jeff@brainn.co>
2021-11-28 13:08:46 -0300

    add first products data fixtures

---
+++ src/data.js
@@ -0,0 +1,128 @@
+export const products = [
+  {
+    id: "1",
+    title: "Useless Git Commit Messages",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image: "https://boyter.org/static/books/CgbR1wkXEAAMjqP.jpg",
+  },
+  {
+    id: "2",
+    title: "Resumé Driven Development",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image: "https://i.imgur.com/DQcks8u.jpg",
+  },
+  {
+    id: "3",
+    title: "Excuses for Not Writing Documentation",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image: "https://boyter.org/static/books/CgGkN6YVAAAZ3_1.jpg",
+  },
+  {
+    id: "4",
+    title: "Turning Coffee into Code",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image:
+      "https://i.pinimg.com/236x/e0/bb/94/e0bb94c01e7c3af661e291012b1ef689--o-reilly-leadership.jpg",
+  },
+  {
+    id: "5",
+    title: "Writing Code that Nobody Else Can Read",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image: "http://miftyisbored.com/wp-content/uploads/orly-writing-code.jpg",
+  },
+  {
+    id: "6",
+    title: "SaaS Addiction",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image:
+      "https://i.pinimg.com/736x/f7/c7/6a/f7c76a13f548d248a83328eaba1134f8.jpg",
+  },
+  {
+    id: "7",
+    title: "Googlin the Error Message",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image:
+      "http://miftyisbored.com/wp-content/uploads/orly-googling-error-messages.jpg",
+  },
+  {
+    id: "8",
+    title: "Taking on Needless Dependencies",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image:
+      "http://miftyisbored.com/wp-content/uploads/orly-needless-dependencies.jpg",
+  },
+  {
+    id: "9",
+    title: "Blaming the user",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image:
+      "https://i.pinimg.com/474x/ce/b2/01/ceb20160325047406f1982b165ce832e--programming-humor-software-development.jpg",
+  },
+  {
+    id: "10",
+    title: "Good Enough to Ship",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image: "https://boyter.org/static/books/Cg-zWsMWwAEJ-Zn.jpg",
+  },
+  {
+    id: "11",
+    title: "Whiteboard Interviews",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image:
+      "http://miftyisbored.com/wp-content/uploads/orly-whiteboard-interviews.png",
+  },
+  {
+    id: "12",
+    title: "Bloated JabbaScript Frameworks",
+    price: 50000,
+    rating: 2,
+    description:
+      "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
+    tags: ["Leve", "bonito", "barato"],
+    image: "https://boyter.org/static/books/ChnulxvW0AEUs1J.jpg-large.jpg",
+  },
+];
