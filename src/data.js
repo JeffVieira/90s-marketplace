e98b1b16fe70042913f4af6f9f9fabd06e366323
Jeff.vieira <jeff@brainn.co>
2021-11-29 10:43:51 -0300

    improve data fixture

--- src/data.js
+++ src/data.js
@@ -2,127 +2,127 @@ export const products = [
   {
     id: "1",
     title: "Useless Git Commit Messages",
-    price: 50000,
+    price: 300,
     rating: 2,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image: "https://boyter.org/static/books/CgbR1wkXEAAMjqP.jpg",
+    onOffer: true,
   },
   {
     id: "2",
     title: "Resumé Driven Development",
-    price: 50000,
-    rating: 2,
+    price: 200,
+    rating: 1,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image: "https://i.imgur.com/DQcks8u.jpg",
+    onOffer: true,
   },
   {
     id: "3",
     title: "Excuses for Not Writing Documentation",
-    price: 50000,
-    rating: 2,
+    price: 350,
+    rating: 3,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image: "https://boyter.org/static/books/CgGkN6YVAAAZ3_1.jpg",
+    onOffer: false,
   },
   {
     id: "4",
     title: "Turning Coffee into Code",
-    price: 50000,
-    rating: 2,
+    price: 1000,
+    rating: 4,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image:
       "https://i.pinimg.com/236x/e0/bb/94/e0bb94c01e7c3af661e291012b1ef689--o-reilly-leadership.jpg",
+    onOffer: false,
   },
   {
     id: "5",
     title: "Writing Code that Nobody Else Can Read",
-    price: 50000,
+    price: 200,
     rating: 2,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image: "http://miftyisbored.com/wp-content/uploads/orly-writing-code.jpg",
+    onOffer: true,
   },
   {
     id: "6",
     title: "SaaS Addiction",
-    price: 50000,
-    rating: 2,
+    price: 200,
+    rating: 1,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image:
       "https://i.pinimg.com/736x/f7/c7/6a/f7c76a13f548d248a83328eaba1134f8.jpg",
+    onOffer: true,
   },
   {
     id: "7",
     title: "Googlin the Error Message",
-    price: 50000,
-    rating: 2,
+    price: 3000,
+    rating: 3,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image:
       "http://miftyisbored.com/wp-content/uploads/orly-googling-error-messages.jpg",
+    onOffer: false,
   },
   {
     id: "8",
     title: "Taking on Needless Dependencies",
-    price: 50000,
+    price: 300,
     rating: 2,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image:
       "http://miftyisbored.com/wp-content/uploads/orly-needless-dependencies.jpg",
+    onOffer: true,
   },
   {
     id: "9",
     title: "Blaming the user",
-    price: 50000,
-    rating: 2,
+    price: 5000,
+    rating: 5,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image:
       "https://i.pinimg.com/474x/ce/b2/01/ceb20160325047406f1982b165ce832e--programming-humor-software-development.jpg",
+    onOffer: false,
   },
   {
     id: "10",
     title: "Good Enough to Ship",
-    price: 50000,
-    rating: 2,
+    price: 5000,
+    rating: 5,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image: "https://boyter.org/static/books/Cg-zWsMWwAEJ-Zn.jpg",
+    onOffer: false,
   },
   {
     id: "11",
     title: "Whiteboard Interviews",
-    price: 50000,
+    price: 500,
     rating: 2,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image:
       "http://miftyisbored.com/wp-content/uploads/orly-whiteboard-interviews.png",
+    onOffer: true,
   },
   {
     id: "12",
     title: "Bloated JabbaScript Frameworks",
-    price: 50000,
-    rating: 2,
+    price: 5000,
+    rating: 5,
     description:
       "Descricao do controle tem que ser mais de u mparagrafo para ficar bacana na UI",
-    tags: ["Leve", "bonito", "barato"],
     image: "https://boyter.org/static/books/ChnulxvW0AEUs1J.jpg-large.jpg",
+    onOffer: false,
   },
 ];
