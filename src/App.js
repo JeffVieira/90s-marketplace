3e082bda1348e481a094c158a904a943f81e9a45
Jeff.vieira <jeff@brainn.co>
2021-11-29 10:45:55 -0300

    remove images

--- src/App.js
+++ src/App.js
@@ -1,94 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home, Cart } from "./pages";
+import { CartProvider } from "./components";
 
 import "antd/dist/antd.css";
 
-import pictureA from "./a.jpg";
-import pictureB from "./b.jpg";
-
-function cartItems() {
-  return [];
-}
-
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
-  );
-
-  return (
-    <main>
-      <header>
-        90s shop
-        <nav>
-          <ul style={{ listStyleType: "none", display: "flex" }}>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            |
-            <li>
-              <a href="/cart">Cart ({cartItems().length})</a>
-            </li>
-          </ul>
-        </nav>
-        <hr />
-      </header>
-
-      {window.location.pathname === "/" && (
-        <div>
-          Welcome to our shop!
-          <p>
-            You are probably interested in <a href="/products/a">A</a>.
-          </p>
-          <p>
-            Check out the newest product <a href="/products/b">B</a>!
-          </p>
-        </div>
-      )}
-      {window.location.pathname === "/products/b" && (
-        <div>
-          <h1>Product B</h1>
-          <p>Price: 30 USD</p>
-
-          <button onClick={() => console.warn("Not implemented!")}>
-            Add to cart
-          </button>
-
-          <div>
-            <img src={pictureB} width={640} />
-          </div>
-        </div>
-      )}
-      {window.location.pathname === "/products/a" && (
-        <div>
-          <h1>Product A</h1>
-          <p>Price: 10 USD</p>
-
-          <button onClick={() => console.warn("Not implemented!")}>
-            Add to cart
-          </button>
-
-          <div>
-            <img src={pictureA} width={640} />
-          </div>
-        </div>
-      )}
-      {window.location.pathname === "/cart" && (
-        <div>
-          Are you ready to purchase these?
-          <ul>
-            {cartItems().map((cartItem) => (
-              <li key={cartItem}>{cartItem}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </main>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 };
 
